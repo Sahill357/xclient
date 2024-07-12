a4028300db3e8a2118fd28184afebc9ce92d1ac9
Your Name <your.email@example.com>
2024-07-12 12:25:19 +0530

    updated

--- clients/api.ts
+++ clients/api.ts
@@ -1,17 +1,16 @@
-import { GraphQLClient } from "graphql-request";
+import { GraphQLClient } from 'graphql-request';
 
- 
+
+
 const isClient = typeof window !== "undefined";
 
 export const graphqlClient = new GraphQLClient(
-    "http://localhost:8000/graphql",
-{
-    headers:()=>({
-        Authorization: isClient
+  process.env.NEXT_PUBLIC_API_URL as string,
+  {
+    headers: () => ({
+      Authorization: isClient
         ? `Bearer ${window.localStorage.getItem("__twitter_token")}`
-        :"",
-
-        
+        : "",
     }),
-}
+  }
 );
--- hooks/tweet.ts
+++ hooks/tweet.ts
@@ -3,25 +3,23 @@ import { CreateTweetData } from "@/gql/graphql";
 import { createTweetMutation } from "@/graphql/mutation/tweet";
 import { getAllTweetsQuery } from "@/graphql/query/tweet";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import toast from "react-hot-toast";
-
-
+import { toast } from "react-hot-toast";
 
 export const useCreateTweet = () => {
   const queryClient = useQueryClient();
 
-
-  const mutation = useMutation ({
-    mutationFn: (payload:CreateTweetData)=>
-      graphqlClient.request(createTweetMutation, {payload}),
-    onMutate: () => toast.loading('Creating Tweet', {id: "1"}),
-    onSuccess: async (payload)=> { await queryClient.invalidateQueries(["all-Tweets"])
-    toast.success('Created Success', {id: "1"})
-  },
+  const mutation = useMutation({
+    mutationFn: (payload: CreateTweetData) =>
+      graphqlClient.request(createTweetMutation, { payload }),
+    onMutate: (payload) => toast.loading("Creating Tweet", { id: "1" }),
+    onSuccess: async (payload) => {
+      await queryClient.invalidateQueries({ queryKey: ["all-tweets"] });
+      toast.success("Created Success", { id: "1" });
+    },
   });
-   
+
   return mutation;
-}
+};
 
 export const useGetAllTweets = () => {
   const query = useQuery({
