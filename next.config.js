79395852417a44107f6b9496c7eeacd075215298
Your Name <your.email@example.com>
2024-07-22 14:10:43 +0530

    new

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
-  images:{
-    domains:["avatars.githubusercontent.com", "lh3.googleusercontent.com","sam-x-dev.s3.ap-south-1.amazonaws.com"]
+  images: {
+    domains: [
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+      "sam-x-dev.s3.ap-south-1.amazonaws.com"
+    ]
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './')
+    };
+    return config;
   }
-}
+};
 
-
-
-module.exports = nextConfig
+module.exports = nextConfig;
