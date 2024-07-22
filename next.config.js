d48a1bdb1dc35f7375b87aae29e326eed3c27302
Your Name <your.email@example.com>
2024-07-22 13:21:53 +0530

    new

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com", "sam-x-dev.s3.ap-south-1.amazonaws.com"],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
+  images:{
+    domains:["avatars.githubusercontent.com", "lh3.googleusercontent.com","sam-x-dev.s3.ap-south-1.amazonaws.com"]
+  }
+}
 
-module.exports = nextConfig;
+
+
+module.exports = nextConfig
