32cf1feba3f4a861c68354e969f5fafb07424bfc
Sahil-Sam <itsmesahil357@gmail.com>
2024-07-27 14:27:58 +0530

    Update type declarations and configuration

--- clients/api.ts
+++ clients/api.ts
@@ -1,14 +1,15 @@
 import { GraphQLClient } from 'graphql-request';
 
+
 const isClient = typeof window !== "undefined";
 
 export const graphqlClient = new GraphQLClient(
   process.env.NEXT_PUBLIC_API_URL as string,
   {
-    headers: {
+    headers: () => ({
       Authorization: isClient
         ? `Bearer ${window.localStorage.getItem("__twitter_token")}`
         : "",
-    }
+    }),
   }
 );
--- types/graphql-request.d.ts
+++ types/graphql-request.d.ts
@@ -1,10 +1,2 @@
-// types/graphql-request.d.ts
-declare module 'graphql-request' {
-    export class GraphQLClient {
-      constructor(endpoint: string, options?: {
-        headers?: Record<string, string>;
-      });
-      request<T>(query: string, variables?: Record<string, any>): Promise<T>;
-    }
-  }
-  
+// graphql-request.d.ts
+declare module 'graphql-request';
