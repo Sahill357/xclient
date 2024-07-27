debb49d90d72109d646a32434717ca6ed0931e6a
Sahil-Sam <itsmesahil357@gmail.com>
2024-07-27 14:19:14 +0530

    Update type declarations and configuration

--- types/graphql-request.d.ts
+++ types/graphql-request.d.ts
@@ -1,2 +1,10 @@
-// graphql-request.d.ts
-declare module 'graphql-request';
+// types/graphql-request.d.ts
+declare module 'graphql-request' {
+    export class GraphQLClient {
+      constructor(endpoint: string, options?: {
+        headers?: Record<string, string>;
+      });
+      request<T>(query: string, variables?: Record<string, any>): Promise<T>;
+    }
+  }
+  
