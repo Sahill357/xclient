d92bf8b2640c34f3ca42464241725ca04dde9156
Sahil-Sam <itsmesahil357@gmail.com>
2024-07-31 00:09:28 +0530

    new

--- graphql/query/user.ts
+++ graphql/query/user.ts
@@ -1,102 +1,91 @@
-import {graphql} from '../../gql';
+import { graphql } from "../../gql";
 
 export const verifyUserGoogleTokenQuery = graphql(`
-#graphql
-query verifyUserGoogleToken($token: String!) {
-  verifyGoogleToken(token: $token)
-}
-
+  #graphql
+  query verifyUserGoogleToken($token: String!) {
+    verifyGoogleToken(token: $token)
+  }
 `);
 
-export const getCurrentUserQuery = graphql (
-    `
+export const getCurrentUserQuery = graphql(
+  `
     query getCurrentUser {
       getCurrentUser {
-        id  
+        id
         profileImageURL
         email
         firstName
         lastName
 
-        
-    recommendedUsers {
-    id
-    firstName
-    lastName
-    profileImageURL
-    }
+        recommendedUsers {
+          id
+          firstName
+          lastName
+          profileImageURL
+        }
 
- followers {
-    id
-    firstName
-    lastName
-    profileImageURL
-    }
+        followers {
+          id
+          firstName
+          lastName
+          profileImageURL
+        }
 
-    following {
-    id
-    firstName
-    lastName
-    profileImageURL
-    }
+        following {
+          id
+          firstName
+          lastName
+          profileImageURL
+        }
 
         tweets {
           id
           content
-          author{
-          id
+          author {
+            id
             firstName
             lastName
             profileImageURL
           }
-
         }
-        }
+      }
     }
-    `
-  
+  `
 );
 
-export const getUserByIdQuery = graphql (`
-
-#graphql  
-query GetuserbyId ($id: ID!) {
-  getUserById(id: $id) {
-    id
-    firstName
-    lastName
-    profileImageURL
-
-
-    followers {
-    id
-    firstName
-    lastName
-    profileImageURL
-    }
-
-    following {
-    id
-    firstName
-    lastName
-    profileImageURL
-    }
-
-    tweets {
-      content
+export const getUserByIdQuery = graphql(`
+  #graphql
+  query GetuserbyId($id: ID!) {
+    getUserById(id: $id) {
       id
-      author {
-      id
+      firstName
+      lastName
+      profileImageURL
+
+      followers {
+        id
         firstName
         lastName
         profileImageURL
       }
+
+      following {
+        id
+        firstName
+        lastName
+        profileImageURL
+      }
+
+      tweets {
+        content
+        id
+        author {
+          id
+          firstName
+          lastName
+          profileImageURL
+        }
+      }
     }
-
   }
-}
-
-`) ;
-
-
-
+`);
